487177579e481d69016d81b35cf0018121006070
Mael Lacour <public.dev@maellacour.com>
2022-12-04 19:40:52 +0100

    Solve first day

---
+++ AdventOfCode/Day_01.cs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode;
+
+using System;
+using System.Collections.Generic;
+
+public class Day01 : BaseDay
+{
+    private readonly int[] _input;
+
+    public Day01()
+    {
+        string[] lines = File.ReadAllLines(InputFilePath);
+        List<int> input = new List<int>();
+        int elfIndex = 0;
+        input.Add(0);
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (lines[i] == string.Empty)
+            {
+                elfIndex++;
+                input.Add(0);
+            }
+            else
+            {
+                input[elfIndex] += int.Parse(lines[i]);
+            }
+        }
+        _input = input.ToArray();
+        Console.Write(_input.ToString());
+    }
+
+    public override ValueTask<string> Solve_1() => new(_input.Max().ToString());
+
+    public override ValueTask<string> Solve_2()
+    {
+        // Shortest but NOT most efficient solution
+        List<int> sortedInput = _input.ToList();
+        sortedInput.Sort();
+        return new((sortedInput[sortedInput.Count - 1] + sortedInput[sortedInput.Count - 2] + sortedInput[sortedInput.Count - 3]).ToString());
+    }
+}
